eae6b21369e9eb22bd7e8ab8bcc29fe20ed9b457
CookieMonsta89 <jlstan06@hotmail.com>
2019-02-15 10:34:32 -0500

    added all flight vehicles

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -5,17 +5,30 @@
 #      v                v
 
 class Vehicle:                       #    <===== BASE CLASS
-    def __init__(self, name, level):
+    def __init__(self, name):
         self.name = name
-        self.type = level 
 
 class FlightVehicle(Vehicle):        #    <===== SUBCLASS to BASE CLASS => VEHICLE
-    def __init__(self):
-        super().__init__(name, level)
+    def __init__(self, name, year_built):
+        super().__init__(name, 1943)
+        self.year_built = year_built
+
+
+class Starship(FlightVehicle):
+    def __init__(self, name):
+        super().__init__(self, name, 2044)      #         <===== SUBCLASS to SUBCLASS => FLIGHTVEHICLE
+
+
+class Airplane(FlightVehicle):
+    def __init__(self, name):
+        super().__init__(self, name, 1902)
 # [GroundVehicle]      [Airplane]
 #   |       |
 #   v       v
 # [Car]  [Motorcycle]
+class GroundVehicle:
+    def __init__(self, name, wheels):
+
 #
 # Each class can simply "pass" for its body. The exercise is about setting up
 # the hierarchy.
